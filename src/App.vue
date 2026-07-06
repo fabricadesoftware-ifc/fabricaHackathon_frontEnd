03a7b3b53dd9e74ebf71e70c24186dba04b718dd
Pedrojoseroza <pedrojs.ol.daroza@gmail.com>
2026-07-06 18:45:40 -0300

    FEAT: Quase completa a barra de carregamento

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,14 @@
 <script lang="ts" setup>
-  import EventCard from './components/ui/eventCard.vue';
-  import StatusProject from './components/ui/statusProject.vue';
-  import ProjectCard from './components/ui/projectCard.vue';
-
+  import ProgressStepper from './components/ui/progressStepper.vue';
 </script>
 
 <template>
-  <div>
-    <div>
-        <EventCard
-            endDate="2 novembro"
-            image="/images/hackifc.png"
-            location="Fabrica"
-            startDate="1 de Novembro"
-            status="inscricoes"
-            statusLabel="Inscrições"
-            :tags="['Criatividade', 'Performance', 'Inovação']"
-            :teams="8"
-            title="HackIFC // 2026"
-        />
-    </div>
-  </div>
 
   <div>
-    <StatusProject
-      label="Ativo"
-      status="ativo"
-    />
+    <ProgressStepper :steps="['Inscrições', 'Andamento', 'Avaliação', 'Finalizado']" :currentStep="4">
+    </ProgressStepper>
   </div>
-
-  <div>
-    <ProjectCard
-        logo="/images/logo.png"
-        status="ativo"
-        statusLabel="Ativo"
-        description="O alugaê é um sistema desenvolvido com intuito de ajudar as pessoas"
-        title="Alugaê"
-        team="Os melhores"
-        event="HackIFC // 2026 · Fabrica"
-    />
-  </div>
+  
 </template>
 
   <style>
---
+++ src/components/ui/progressStepper.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps<{
+    steps: string[]
+    currentStep: 0 | 1 | 2 | 3 | 4
+}>()
+</script>
+
+<template>
+    <div class="p-5 bg-gray-600 pl-8 pr-8 w-[70vw] flex justify-evenly items-center">
+        <template v-for="i in 4" :key="i">
+
+            <div class="grid grid-cols-1 place-items-center">
+                <div class="rounded-full mt-5 w-10 h-10  flex justify-center items-center"
+                    :class="i - 1 <= currentStep ? 'bg-blue-700 border-0' : 'bg-white border-gray-400 border-4'">
+                    <span class="mdi mdi-check text-2xl" v-show="currentStep > i-1"></span>
+                    <span v-show="currentStep == i-1">{{ i }}</span>
+                </div>
+                <span class="mt-1 text-[1.1rem]" :class="currentStep >= i-1? 'text-blue-700' : 'text-gray-400'">
+                    {{ steps[i-1] }}
+                </span>
+
+            </div>
+            <div class="w-45 h-1.5" :class="currentStep > i - 1 ? 'bg-blue-700' : 'bg-gray-400'" v-show="i !== 4">
+            </div>
+        </template>
+    </div>
+</template>
+
+<style scoped></style>
