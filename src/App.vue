77c458b8ddd95c5296f4007811d075639675c478
arthurlanz <arthurlanznaster@gmail.com>
2026-07-11 15:45:32 -0300

    Feat#10-Integrar Sidebar no App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,58 @@
 <script lang="ts" setup>
-  import EventCard from './components/ui/eventCard.vue';
-  import StatusProject from './components/ui/statusProject.vue';
-  import ProjectCard from './components/ui/projectCard.vue';
+  import { ref } from 'vue'
+  import EventCard from './components/ui/eventCard.vue'
+  import ProjectCard from './components/ui/projectCard.vue'
+  import { Sidebar } from './components/ui/Sidebar'
+  import StatusProject from './components/ui/statusProject.vue'
+
+  const activeItem = ref('home')
+  const isLoggedIn = ref(true)
 
 </script>
 
 <template>
-  <div>
-    <div>
-        <EventCard
-            endDate="2 novembro"
+  <div class="flex">
+    <Sidebar v-model:active-item="activeItem" :is-logged-in="isLoggedIn" />
+
+    <main class="flex-1">
+      <div>
+        <div>
+          <EventCard
+            end-date="2 novembro"
             image="/images/hackifc.png"
             location="Fabrica"
-            startDate="1 de Novembro"
+            start-date="1 de Novembro"
             status="inscricoes"
-            statusLabel="Inscrições"
+            status-label="Inscrições"
             :tags="['Criatividade', 'Performance', 'Inovação']"
             :teams="8"
             title="HackIFC // 2026"
+          />
+        </div>
+      </div>
+
+      <div>
+        <StatusProject
+          label="Ativo"
+          status="ativo"
         />
-    </div>
-  </div>
+      </div>
 
-  <div>
-    <StatusProject
-      label="Ativo"
-      status="ativo"
-    />
-  </div>
-
-  <div>
-    <ProjectCard
-        logo="/images/logo.png"
-        status="ativo"
-        statusLabel="Ativo"
-        description="O alugaê é um sistema desenvolvido com intuito de ajudar as pessoas"
-        title="Alugaê"
-        team="Os melhores"
-        event="HackIFC // 2026 · Fabrica"
-    />
+      <div>
+        <ProjectCard
+          description="O alugaê é um sistema desenvolvido com intuito de ajudar as pessoas"
+          event="HackIFC // 2026 · Fabrica"
+          logo="/images/logo.png"
+          status="ativo"
+          status-label="Ativo"
+          team="Os melhores"
+          title="Alugaê"
+        />
+      </div>
+    </main>
   </div>
 </template>
 
-  <style>
+<style>
 
-  </style>
+</style>
