14f99e0e2fba29f96842db1dcf4651dfe9366f8b
Davi-prussek <davi.mariano.prussek0916@gmail.com>
2026-07-15 01:14:22 -0300

    FEAT: Configuração do sistema do input, adição do pacote do vuetify porque ele não tava registrado, que dor de cabeça.

---
+++ src/components/ui/SelectTipo/SelectTipo.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-select
+    v-model="model"
+    item-title="label"
+    item-value="value"
+    :items="props.options"
+    persistent-placeholder
+    :placeholder="props.placeholder"
+  />
+</template>
+<script setup lang="ts">
+  import { computed } from 'vue'
+  import { VSelect } from 'vuetify/components'
+
+  type Model = string | null
+
+  interface SelectOption {
+    label: string
+    value: string
+  }
+
+  const props = defineProps<{
+    options: SelectOption[]
+    modelValue: Model
+    placeholder: string
+  }>()
+
+  const emits = defineEmits<{
+    (e: 'update:modelValue', value: Model): void
+  }>()
+
+  const model = computed({
+    get: () => props.modelValue,
+    set: (value: Model) => emits('update:modelValue', value),
+  })
+</script>
---
+++ src/components/ui/SelectTipo/index.ts
@@ -0,0 +1,1 @@
+export { default } from './SelectTipo.vue'
