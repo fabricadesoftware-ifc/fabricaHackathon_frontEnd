a97d95d1542a76facd4130998061052487c824db
Davi-prussek <davi.mariano.prussek0916@gmail.com>
2026-07-16 01:36:43 -0300

    FIX: Continuação da estilização do SelectTipo e correções e configurações v-select

--- src/components/ui/SelectTipo/SelectTipo.vue
+++ src/components/ui/SelectTipo/SelectTipo.vue
@@ -1,17 +1,19 @@
 <template>
-  <v-select
+  <VSelect
     v-model="model"
-    clearable
-    hide-selected
+    bg-color="white"
+    class="select-tipo max-w-40 max-h-20"
+    flat
+    hide-details
     item-title="label"
     item-value="value"
     :items="props.options"
+    menu-icon="mdi-chevron-down"
     persistent-placeholder
     :placeholder="props.placeholder"
-  >
-    <template #selection>
-    </template>
-  </v-select>
+    rounded="lg"
+    variant="solo"
+  />
 </template>
 <script setup lang="ts">
   import { computed } from 'vue'
@@ -39,3 +41,32 @@
     set: (value: Model) => emits('update:modelValue', value),
   })
 </script>
+<style scoped>
+.v-field {
+  background-color: white;
+}
+
+.select-tipo :deep(.v-field__input) {
+  font-size: 20px;
+  font-weight: 400;
+  color: #111827;
+}
+
+.select-tipo :deep(.v-select__control) {
+
+}
+
+.select-tipo :deep(.v-select__selection) {
+  display: none;
+}
+
+.select-tipo :deep(input) {
+  text-align: center;
+  align-items: center;
+  font-size: 25px;
+}
+
+.select-tipo :deep(.v-field__append-inner) {
+padding-right: 1.2vw;
+}
+</style>
