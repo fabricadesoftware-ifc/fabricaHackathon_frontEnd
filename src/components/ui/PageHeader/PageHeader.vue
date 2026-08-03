22e1df2a027ca03e52996d872a462d262ad74ffa
mklamaral <mikael.a.amaral07@gmail.com>
2026-08-03 14:40:24 -0300

    Adicionando propriedades responsivas ao componente PageHeader

--- src/components/ui/PageHeader/PageHeader.vue
+++ src/components/ui/PageHeader/PageHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col gap-1 my-4">
-        <p class="text-gray-900 text-2xl">{{ title }}</p>
-        <p class="text-gray-600 text-base">{{ subtitle }}</p>
+        <p class="text-gray-900 md:text-2xl">{{ title }}</p>
+        <p class="text-gray-600 md:text-base">{{ subtitle }}</p>
     </div>
 </template>
 
