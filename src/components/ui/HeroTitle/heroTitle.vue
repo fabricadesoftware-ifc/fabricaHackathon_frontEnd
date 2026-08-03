9da3b52fa1e09b672c1d677e0eecfd0e377cff61
pedrojoseroza <pedrojs.ol.daroza@gmail.com>
2026-08-03 14:51:37 -0300

    chore: mais um margin bottom no subtitle

--- src/components/ui/HeroTitle/heroTitle.vue
+++ src/components/ui/HeroTitle/heroTitle.vue
@@ -10,7 +10,7 @@ const props = defineProps<{
     <h1 class="text-[2rem] text-center my-5 text-black">
         {{ props.title }}
     </h1>
-    <h2 class="text-[1.5rem] text-center mb-2 text-[#565F6D] ">
+    <h2 class="text-[1.5rem] text-center mb-7 text-[#565F6D] ">
         {{ props.subtitle }}
     </h2>    
   </div>
