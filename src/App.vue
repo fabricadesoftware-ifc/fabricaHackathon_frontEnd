9899930cb5cdac6d93409d5a08dee4fadc99407d
pedrojoseroza <pedrojs.ol.daroza@gmail.com>
2026-08-03 14:25:48 -0300

    Feat: cria o componente, herosection, o componente heroTitle e o faz uma alteração em  search bar para aumentar um pouco a largura

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,11 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
-import EventCard from './components/ui/eventCard.vue';
-import StatusProject from './components/ui/statusProject.vue';
-import ProjectCard from './components/ui/projectCard.vue';
-import AppButton from './components/ui/AppButton/index.ts';
-import HeaderActions from './components/ui/HeaderActions/index.ts';
-import SearchBar from './components/ui/SearchBar'
-import { Sidebar } from './components/ui/Sidebar'
-
-const activeItem = ref('home')
-const isLoggedIn = ref(true)
+import heroSection from './components/ui/HeroSection/heroSection.vue';
 
 </script>
 <template>
   <div class="flex">
-    <Sidebar v-model:active-item="activeItem" :is-logged-in="isLoggedIn" />
-    <main class="flex-1">
-      <div>
-        <div>
-          <EventCard end-date="2 novembro" image="/images/hackifc.png" location="Fabrica" start-date="1 de Novembro"
-            status="inscricoes" status-label="Inscrições" :tags="['Criatividade', 'Performance', 'Inovação']" :teams="8"
-            title="HackIFC // 2026" />
-        </div>
-      </div>
-
-      <div>
-        <StatusProject label="Ativo" status="ativo" />
-      </div>
-
-      <div>
-        <ProjectCard description="O alugaê é um sistema desenvolvido com intuito de ajudar as pessoas"
-          event="HackIFC // 2026 · Fabrica" logo="/images/logo.png" status="ativo" status-label="Ativo"
-          team="Os melhores" title="Alugaê" />
-      </div>
-      <div class="flex items-center gap-3 p-4">
-        <AppButton variant="text" label="Entrar" @click="() => console.log('entrar clicado')" />
-        <AppButton variant="primary" label="Criar conta" @click="() => console.log('criar conta clicado')" />
-      </div>
-      <HeaderActions />
-      <SearchBar placeholder="Pesquisar hackathons ou projetos..." modelValue="asdasd"></SearchBar>
-    </main>
+    <heroSection></heroSection>
     </div>
 </template>
 <style></style>
---
+++ src/components/ui/HeroSection/heroSection.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import heroTitle from '../HeroTitle/heroTitle.vue';
+import searchBar from '../SearchBar/searchBar.vue';
+const value = ref('');
+</script>
+
+<template>
+  <div class="min-h-40 grid grid-cols-1 justify-items-center center bg-white py-10 min-w-full">
+        <heroTitle title="Desenvolva, colabore e inove no HackIFC // 24H" subtitle="Desenvolva, colabore e inove no HackIFC // 24H"></heroTitle>
+        <searchBar :model-value="value"></searchBar>
+    
+  </div>
+</template>
+
+<style scoped>
+
+</style>
