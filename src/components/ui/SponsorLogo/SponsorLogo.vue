001465654ef1f4f06110f4f481d02618daf304bc
Pedrojoseroza <pedrojs.ol.daroza@gmail.com>
2026-08-08 15:51:23 -0300

    feat: Cria SponsorList e garante comportamento responsivo

--- src/components/ui/SponsorLogo/SponsorLogo.vue
+++ src/components/ui/SponsorLogo/SponsorLogo.vue
@@ -3,16 +3,14 @@ const props = defineProps<{
     imageUrl: string
     href?: string
     name: string
-    maxWidth?: number // Apenas para questões de manipular as imagens
-    maxHeight?: number
-
+   
 }>()
 </script>
 <template>
     <div class=" w-fit bg-white p-1.5 flex justify-center items-center">
         <a :href="props.href" target="_blank" v-if="props.href">    
-        <img class="min-w-[5vw]" :class="`max-w-${props.maxWidth} max-h-${props.maxHeight}`" :src="props.imageUrl" :alt="props.name">
+        <img class="min-w-[5vw]"  :src="props.imageUrl" :alt="props.name">
         </a>
-        <img v-else class="min-w-[5vw]" :class="`max-w-${props.maxWidth} max-h-${props.maxHeight}`" :src="props.imageUrl" :alt="props.name">
+        <img v-else class="min-w-[5vw]"  :src="props.imageUrl" :alt="props.name">
     </div>
 </template>
