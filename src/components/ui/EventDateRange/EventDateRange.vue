f8a861ea06ca9e9d00a606a8edf1ccc8970879af
Davi-prussek <davi.mariano.prussek0916@gmail.com>
2026-08-09 01:29:48 -0300

    FEAT: Criação do DateRange

---
+++ src/components/ui/EventDateRange/EventDateRange.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="text-[16px] flex gap-3.5 bg-[#F3F4F6] w-fit h-min items-center rounded-full px-7">
+    <span class="mdi mdi-calendar text-[30px]" />
+
+    <div class="flex gap-3">
+      <span>{{ props.startDate }}</span>
+      <span>{{ '-' }}</span>
+      <span>{{ props.endDate }}</span>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+  const props = defineProps<{
+    startDate: string
+    endDate: string
+  }>()
+</script>
