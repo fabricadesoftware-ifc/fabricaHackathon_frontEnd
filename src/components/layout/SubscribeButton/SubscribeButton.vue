efa8d626aa531cdfde0af49251a281d9a918d5a7
Davi-prussek <davi.mariano.prussek0916@gmail.com>
2026-08-12 00:47:21 -0300

    STYLE: Correção de tamanho de fonte e de padding

--- src/components/layout/SubscribeButton/SubscribeButton.vue
+++ src/components/layout/SubscribeButton/SubscribeButton.vue
@@ -1,15 +1,16 @@
 <template>
   <button
-    class="bg-[#2563EB] text-[24px] text-white py-1.5 px-6 rounded-md flex items-center gap-2 hover:bg-[#0d4edc] disabled:bg-[#787878] disabled:text-[#d8d8d8]"
-    @click="emit('click')"
+    class="bg-[#2563EB] text-[22px] text-white py-1.5 px-5.5 rounded-md flex items-center gap-2 hover:bg-[#0d4edc] disabled:bg-[#787878] disabled:text-[#d8d8d8]"
   >
+    <!-- @click="emit('click')" -->
+
     <span class="mdi mdi-ticket-outline" /> <p>Inscrever-se</p>
   </button>
 </template>
 <script setup lang="ts">
 
-  const emit = defineEmits<{
+/*   const emit = defineEmits<{
     (e: 'click'): void
-  }>()
+  }>() */
 
 </script>
