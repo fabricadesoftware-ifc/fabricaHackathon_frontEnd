fc7fb41ef594ac5ce5682c1c6dac7b76e6414887
Pedrojoseroza <pedrojs.ol.daroza@gmail.com>
2026-08-16 17:59:44 -0300

    Feat: Cria o summaryRow

---
+++ src/components/ui/SummaryRow/summaryRow.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+
+const props = defineProps<{
+  label: string
+  values: string | string[]
+}>()
+const values = computed(()=>{
+  return Array.isArray(props.values)? props.values : [props.values];
+})
+</script>
+
+<template>
+  <div class="min-w-full flex py-3 px-4 justify-between bg-white shadow-lg my-3 rounded-2xl">
+    <span class="text-gray-500">
+      {{ props.label }}
+    </span>
+    <div class="flex gap-1.5 items-center">
+      <span v-for="(valor, index) in values" :key="index">
+        {{ valor }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
