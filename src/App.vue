0105901cf58efe46d0e0f1a72442b59069e492b4
mklamaral <mikael.a.amaral07@gmail.com>
2026-08-21 14:00:25 -0300

    Feat#108-Adiciona o componente EvaluationCriteriaCard para pontuacao e comentarios

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,83 @@
 <script lang="ts" setup>
-  import { ref, computed } from 'vue'
-  import AvatarUpload from '@/components/layout/AvatarUpload'
-  import AppHeader from './components/layout/AppHeader/AppHeader.vue'
-  import EventDateRange from './components/ui/EventDateRange/EventDateRange.vue'
-  import { Sidebar } from './components/ui/Sidebar'
-import LoginCard from './components/layout/LoginCard/LoginCard.vue'
-import AppButton from './components/ui/AppButton/AppButton.vue'
-import ProgressBar from './components/ui/ProgressBar/ProgressBar.vue'
+import { ref, computed } from "vue";
+import AvatarUpload from "@/components/layout/AvatarUpload";
+import AppHeader from "./components/layout/AppHeader/AppHeader.vue";
+import EventDateRange from "./components/ui/EventDateRange/EventDateRange.vue";
+import { Sidebar } from "./components/ui/Sidebar";
+import LoginCard from "./components/layout/LoginCard/LoginCard.vue";
+import AppButton from "./components/ui/AppButton/AppButton.vue";
+import ProgressBar from "./components/ui/ProgressBar/ProgressBar.vue";
+import EvaluationCriteriaCard from "./components/ui/EvaluationCriteriaCard/EvaluationCriteriaCard.vue";
 
 function salvar() {
-  console.log("Salvou!")
+  console.log("Salvou!");
 }
 
 const remover = (id: number) => {
-  convidados.value = convidados.value.filter(c => c.id !== id)
-}
+  convidados.value = convidados.value.filter((c) => c.id !== id);
+};
 
 const handleLogin = (dados: {
   email: string;
   password: string;
   rememberMe: boolean;
 }) => {
-  console.log('Login:', dados);
+  console.log("Login:", dados);
 };
 
 const handleGoogleLogin = () => {
-  console.log('Login com Google');
+  console.log("Login com Google");
 };
 
 const handleForgotPassword = () => {
-  console.log('Esqueci minha senha');
+  console.log("Esqueci minha senha");
 };
 
 const handleCreateAccount = () => {
-  console.log('Criar conta');
+  console.log("Criar conta");
 };
 
-  const activeItem = ref('home')
-  const isLoggedIn = ref(true)
-  const file: File = { name: '', lastModified: 0, size: 0, type: '', webkitRelativePath: '' }
+const activeItem = ref("home");
+const isLoggedIn = ref(true);
+const file: File = {
+  name: "",
+  lastModified: 0,
+  size: 0,
+  type: "",
+  webkitRelativePath: "",
+};
+
+const notaCriatividade = ref(7.0)
+const comentarioCriatividade = ref('')
 </script>
 <template>
-
   <div class="flex">
     <Sidebar v-model:active-item="activeItem" :is-logged-in="isLoggedIn" />
 
-
     <main class="flex-1 flex-row bg-[#F9FAFB]">
-      <AppHeader :is-logged="true" :notification-count="3" user-avatar="/avatar.png" user-name="Renan" />
+      <AppHeader
+        :is-logged="true"
+        :notification-count="3"
+        user-avatar="/avatar.png"
+        user-name="Renan"
+      />
       <div class="mx-10">
         <RouterView />
+        <EvaluationCriteriaCard
+          criteriaName="Criatividade"
+          v-model="notaCriatividade"
+          v-model:comment="comentarioCriatividade"
+        />
+         <EvaluationCriteriaCard
+          criteriaName="Criatividade"
+          v-model="notaCriatividade"
+          v-model:comment="comentarioCriatividade"
+        />
+         <EvaluationCriteriaCard
+          criteriaName="Criatividade"
+          v-model="notaCriatividade"
+          v-model:comment="comentarioCriatividade"
+        />
       </div>
     </main>
   </div>
